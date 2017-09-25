523ebe0aab4fc51cecc56c2a6bea87125e7312bd
Osma Suominen <osma.suominen@helsinki.fi>
2017-09-25 16:10:39 +0300

    skip files which already have subjects assigned

--- autoindex_txtdir.py
+++ autoindex_txtdir.py
@@ -3,6 +3,7 @@
 # Perform automatic subject indexing for a directory with text files (.txt).
 # If there is a .subj file, assign as many subjects as there are subjects in that file.
 # Place the result in a file with the extension .asubj in TSV format.
+# If the .asubj file already exists, do nothing.
 
 import sys
 import os
@@ -15,6 +16,11 @@ for fn in os.listdir(dir):
     if not fn.lower().endswith('.txt'):
         continue
     id = fn[:-4]
+
+    outfn = os.path.join(dir, "%s.asubj" % id)
+    if os.path.isfile(outfn):
+        continue # done already
+
     f = open(os.path.join(dir, fn))
     text = f.read()
     subjfile = os.path.join(dir, "%s.subj" % id)
@@ -25,7 +31,6 @@ for fn in os.listdir(dir):
     else:
         maxhits = 3
     
-    outfn = os.path.join(dir, "%s.asubj" % id)
     print(fn, len(text), subjfile, maxhits, outfn)
     
     results = autoindex.autoindex(text, 'yso-finna-fi', threshold=0.1, maxhits=maxhits)
